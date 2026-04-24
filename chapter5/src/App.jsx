b832a832439df7e3b87b57199b6daded912079fd
seungjin <xeungjin@nate.com>
2026-04-24 13:03:47 +0900

    ~ 192 page

--- chapter5/src/App.jsx
+++ chapter5/src/App.jsx
@@ -1,12 +1,27 @@
 import './App.css'
 
+const Header2 = () => {
+  return (
+    <header>
+      <h1>header2</h1>
+    </header>
+  )
+}
+
+function Header() {
+  return (
+    <header>
+      <h1>header</h1>
+    </header>
+  );
+}
+
 function App() {
   return (
-    <>
-      <div>
-
-      </div>
-    </>
+    <div>
+      <Header />
+      <Header2 />
+    </div>
   )
 }
 
