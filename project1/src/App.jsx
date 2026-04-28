fdcb6eeba395a2cd9949454d4319b7b4178dbf4d
seungjin <xeungjin@nate.com>
2026-04-28 16:39:19 +0900

    ~277page

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -1,16 +1,35 @@
 import "./App.css";
-import { useState } from "react";
+import { useRef, useEffect, useState } from "react";
 import Controller from "./component/Controller";
 import Viewer from "./component/Viewer";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const didMountRef = useRef(false);
+
   const handleSetCount = (value) => {
     setCount(count + value);
   };
+  const handleChangeText = (e) => {
+    setText(e.target.value);
+  };
+
+  useEffect(() => {
+    if (!didMountRef.current) {
+      didMountRef.current = true;
+      return;
+    } else {
+      console.log("컴포넌트 업데이트!");
+    }
+  });
+
   return (
     <div className="App">
       <h1>Simple Counter</h1>
+      <section>
+        <input type={text} onChange={handleChangeText} />
+      </section>
       <section>
         <Viewer count={count} />
       </section>
