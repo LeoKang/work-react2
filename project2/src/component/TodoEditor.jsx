1428f8f1dfd788387e46f4e5815b5a2f113a0d9d
seungjin <xeungjin@nate.com>
2026-04-29 15:45:48 +0900

    332page

--- project2/src/component/TodoEditor.jsx
+++ project2/src/component/TodoEditor.jsx
@@ -16,14 +16,21 @@ const TodoEditor = ({ onCreate }) => {
     onCreate(content);
     setContent("");
   };
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
   return (
     <div className="TodoEditor">
-      <h4>새로운 Todo 작성하기</h4>
+      <h4>새로운 Todo 작성하기🖋️</h4>
       <div className="editor_wrapper">
         <input
           ref={inputRef}
           value={content}
           onChange={onChangeContent}
+          onKeyDown={onKeyDown}
           placeholder="새로운 Todo..."
         />
         <button onClick={onSubmit}>추가</button>
