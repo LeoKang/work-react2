062597562879eba505fb2852e2646f186446fe50
seungjin <xeungjin@nate.com>
2026-05-07 11:40:37 +0900

    466page

--- project3/src/component/Editor.jsx
+++ project3/src/component/Editor.jsx
@@ -1,6 +1,6 @@
 import "./Editor.css";
 import { useState } from "react";
-import { getFormattedDate } from "../util";
+import { emotionList, getFormattedDate } from "../util";
 import Button from "./Button";
 import { useNavigate } from "react-router-dom";
 
@@ -44,8 +44,12 @@ const Editor = ({ initData, onSubmit }) => {
         </div>
       </div>
       <div className="editor_section">
-        {/* 감정 */}
         <h4>오늘의 감정</h4>
+        <div className="input_wrapper emotion_list_wrapper">
+          {emotionList.map((it) => (
+            <img key={it.id} alt={`emotion${it.id}`} src={it.img} />
+          ))}
+        </div>
       </div>
       <div className="editor_section">
         {/* 일기 */}
