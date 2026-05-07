699a9cfc9d15b9273d9c0001e7af0d614ac64596
seungjin <xeungjin@nate.com>
2026-05-07 15:53:40 +0900

    486page

--- project3/src/App.jsx
+++ project3/src/App.jsx
@@ -1,11 +1,87 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Edit from "./pages/Edit";
+import { useReducer, useRef, useEffect } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "INIT":
+      return action.data;
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((it) =>
+        String(it.id) === String(action.data.id) ? { ...action.data } : it,
+      );
+    case "DELETE":
+      return state.filter((it) => String(it.id) !== String(action.targetId));
+    default: {
+      return state;
+    }
+  }
+}
+
+const mockData = [
+  {
+    id: "mock1",
+    date: new Date().getTime(),
+    content: "mock1",
+    emotionId: 1,
+  },
+  {
+    id: "mock2",
+    date: new Date().getTime(),
+    content: "mock2",
+    emotionId: 2,
+  },
+];
 
 function App() {
+  const [data, dispatch] = useReducer(reducer, []);
+  const idRef = useRef(0);
+
+  useEffect(() => {
+    dispatch({
+      type: "INIT",
+      data: mockData,
+    });
+  }, []);
+
+  const onCreate = (date, content, emotionId) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current,
+        date: new Date(date).getTime(),
+        content,
+        emotionId,
+      },
+    });
+    idRef.current += 1;
+  };
+
+  const onUpdate = (targetId, date, content, emotionId) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotionId,
+      },
+    });
+  };
+
+  const onDelete = (targetId) => {
+    dispatch({
+      type: "DELETE",
+      targetId,
+    });
+  };
+
   return (
     <div className="App">
       <Routes>
@@ -14,12 +90,6 @@ function App() {
         <Route path="/diary/:id" element={<Diary />} />
         <Route path="/edit" element={<Edit />} />
       </Routes>
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>New</Link>
-        <Link to={"/diary"}>Diary</Link>
-        <Link to={"/edit"}>Edit</Link>
-      </div>
     </div>
   );
 }
