3ef83a02cd773f9a1329fa394581dd672e834622
seungjin <xeungjin@nate.com>
2026-05-07 17:27:31 +0900

    495page

--- project3/src/pages/Home.jsx
+++ project3/src/pages/Home.jsx
@@ -1,4 +1,27 @@
+import { useState } from "react";
+import Button from "../component/Button";
+import Header from "../component/Header";
+
 const Home = () => {
-  return <div>Home 페이지입니다</div>;
+  const [pivotDate, setPivotDate] = useState(new Date());
+  const headerTitle = `${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`;
+
+  const onIncreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+  };
+
+  const onDecreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+  };
+
+  return (
+    <div>
+      <Header
+        title={headerTitle}
+        leftChild={<Button text={"<"} />}
+        rightChild={<Button text={">"} />}
+      />
+    </div>
+  );
 };
 export default Home;
