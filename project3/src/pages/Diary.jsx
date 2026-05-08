fcf017210b8d0fc81deeaa1e2083fadb3ec5c2a6
seungjin <xeungjin@nate.com>
2026-05-08 16:43:21 +0900

    523페이지

--- project3/src/pages/Diary.jsx
+++ project3/src/pages/Diary.jsx
@@ -4,11 +4,17 @@ import useDiary from "../hooks/useDiary";
 const Diary = () => {
   const { id } = useParams();
   const data = useDiary(id);
-  return (
-    <div>
-      <div>{id}번 일기</div>
-      <div>Diary 페이지입니다</div>
-    </div>
-  );
+  console.log(data);
+
+  if (!data) {
+    return <div>일기를 불러오고 있습니다...</div>;
+  } else {
+    return (
+      <div>
+        <div>{id}번 일기</div>
+        <div>Diary 페이지입니다</div>
+      </div>
+    );
+  }
 };
 export default Diary;
