28d30b6ba91b19ecec936a85f3a70457cec4ff18
seungjin <xeungjin@nate.com>
2026-05-08 10:18:51 +0900

    497page

--- project3/src/pages/Home.jsx
+++ project3/src/pages/Home.jsx
@@ -1,12 +1,14 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { DiaryStateContext } from "../App";
 import Button from "../component/Button";
 import Header from "../component/Header";
+import { getMonthRangeByDate } from "../util";
 
 const Home = () => {
   const [pivotDate, setPivotDate] = useState(new Date());
   const headerTitle = `${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`;
   const data = useContext(DiaryStateContext);
+  const [filteredData, setFilteredData] = useState([]);
 
   const onIncreaseMonth = () => {
     setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
@@ -15,6 +17,19 @@ const Home = () => {
   const onDecreaseMonth = () => {
     setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
   };
+
+  useEffect(() => {
+    if (data.length >= 1) {
+      const { beginTimeStamp, endTimeStamp } = getMonthRangeByDate(pivotDate);
+      setFilteredData(
+        data.filter(
+          (it) => beginTimeStamp <= it.date && it.date <= endTimeStamp,
+        ),
+      );
+    } else {
+      setFilteredData([]);
+    }
+  }, [data, pivotDate]);
 
   return (
     <div>
