eeb524dbb5390ce07bfbbfe82ec97c59329ebe3f
seungjin <xeungjin@nate.com>
2026-05-08 17:39:09 +0900

    530page

--- project3/src/component/Viewer.jsx
+++ project3/src/component/Viewer.jsx
@@ -5,6 +5,16 @@ const Viewer = ({ content, emotionId }) => {
   const emotionItem = emotionList.find((it) => it.id === emotionId);
   console.log(emotionItem);
 
-  return <div className="Viewer"></div>;
+  return (
+    <div className="Viewer">
+      <section>
+        <h4>오늘의 감정</h4>
+        <div className={`emotion_img_wrapper emotion_img_wrapper_${emotionId}`}>
+          <img alt={emotionItem.name} src={emotionItem.img} />
+          <div className="emotion_descript">{emotionItem.name}</div>
+        </div>
+      </section>
+    </div>
+  );
 };
 export default Viewer;
