f85da370eab7f0ba7175c4d90930c435cc4791b0
seungjin <xeungjin@nate.com>
2026-05-11 10:37:12 +0900

    536page

--- project3/src/pages/New.jsx
+++ project3/src/pages/New.jsx
@@ -1,11 +1,20 @@
 import { useNavigate } from "react-router-dom";
 import Button from "../component/Button";
 import Header from "../component/Header";
+import Editor from "../component/Editor";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
 
 const New = () => {
   const navigate = useNavigate();
+  const { onCreate } = useContext(DiaryDispatchContext);
   const goBack = () => {
     navigate(-1);
+  };
+  const onSubmit = (data) => {
+    const { date, content, emotionId } = data;
+    onCreate(date, content, emotionId);
+    navigate("/", { replace: true });
   };
 
   return (
@@ -14,6 +23,7 @@ const New = () => {
         title={"새 일기 쓰기"}
         leftChild={<Button text={"< 뒤로 가기"} onClick={goBack} />}
       />
+      <Editor onSubmit={onSubmit} />
     </div>
   );
 };
